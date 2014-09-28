8d3a71c327930e6bba2b2100243efad4c8770fac
gayaleemelo <gayalee.melo@gmail.com>
2014-09-28 09:37:20 +0300

    Initial Commit

---
+++ Partner compatability/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Partner compatability
+//
+//  Created by gaya lee melo on 28/09/14.
+//  Copyright (c) 2014 gaya lee melo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
